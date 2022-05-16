16740828ebe8c2602392a183aadcc59bbbe97adc
Morgan <55892175+morganpothoff@users.noreply.github.com>
2022-05-15 23:11:15 -0500

    Initial Commit

---
+++ Source/GroceryGuru.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from flask import *
+from datetime import datetime
+import database.DatabaseConnection
+import json
+import os
+
+#import DB_Connections
+
+
+app = Flask(__name__, static_url_path="/static")
+
+
+@app.route("/")
+def home():
+	return render_template("Home.html")
+
+
+@app.route("/Login")
+def login():
+	return render_template("Login.html")
+
+
+@app.route("/CreateAccount")
+def createAccount():
+	return render_template("CreateAccount.html")
+
+
+@app.route("/MyPantry")
+def myPantry():
+	return render_template("MyPantry.html")
+
+
+@app.route("/About")
+def about():
+	return render_template("About.html")
+
+
+@app.route("/MyAccount")
+def myAccount():
+	return render_template("AccountInfo.html")
+
+
+app.run(host="localhost", port=8000, debug=True)
