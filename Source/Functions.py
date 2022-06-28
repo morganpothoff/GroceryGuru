3d4229e48fb703fa01813f66b025c7f1542d02a4
mpzinke <mathew.zinke@hotmail.com>
2022-06-27 21:05:34 -0500

    Adds exceptions for data that isn't found & wraps connection functions

--- Source/Functions.py
+++ Source/Functions.py
@@ -2,64 +2,103 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import *
-from datetime import datetime
-import database.DatabaseConnection
-import json
 import os
 import psycopg2
+import werkzeug
 
 
 # Connects to the database
-def new_database_connection():
-	DB_USER = os.getenv("GROCERY_GURU_DB_USER")
-	DB_PASSWORD = os.getenv("GROCERY_GURU_DB_PASSWORD")
-	connection = psycopg2.connect(F"host=localhost dbname=GroceryGuru user={DB_USER} password={DB_PASSWORD}")
-	return connection
+def close(*connections: list) -> None:
+	"""
+	SUMMARY: Closes all passed connections.
+	DETAILS: Iterates through connections, calling the close method on each connection.
+	"""
+	for connection in connections:
+		connection.close()
 
 
-# Determines if user exists from email or uname
-def user_exists(request):
-	connection = new_database_connection()
-	connection.autocommit = True	# Set auto commit to false
-	cursor = connection.cursor()
-	
-	cursor.execute("""SELECT * FROM "Persons" WHERE "Email" = %s;""", (request.form["email"],))
-	number_of_results_with_matching_Email = cursor.rowcount
-	if number_of_results_with_matching_Email > 0:
-		return True
+def connect(function: callable) -> callable:
+	"""
+	SUMMARY: Wraps functions with function to create cursor for DB and close connections when done it.
+	DETAILS: Gets the environment variable for the DB's connection. Connects to the Postgresql DB. Creates a cursor for
+	         the connection. Passes it to the callback. Closes connection to DB.
+	RETURNS: The wrapped function with the cursor for the established connection.
+	"""
+	def inner(*args: list, **kwargs: dict) -> callable:
+		DB_USER = os.getenv("GROCERY_GURU_DB_USER")
+		DB_PASSWORD = os.getenv("GROCERY_GURU_DB_PASSWORD")
+		assert(DB_USER is not None), "'DB_USER' not set"
+		assert(DB_PASSWORD is not None), "'DB_PASSWORD' not set"
+		connection_string = f"host=localhost dbname=GroceryGuru user={DB_USER} password={DB_PASSWORD}"
 
-	cursor.execute("""SELECT * FROM "Persons" WHERE "Username" = %s;""", (request.form["uname"],))
-	number_of_results_with_matching_Username = cursor.rowcount
-	if number_of_results_with_matching_Email > 0:
-		return True
+		connection = psycopg2.connect(connection_string)
+		connection.autocommit = True  # Automatically commit changes to DB
+		cursor = connection.cursor()
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	return False
+		try:
+			function_result = function(cursor, *args, **kwargs)
+			close(cursor, connection)
+			return function_result
+
+		except Exception as error:
+			close(cursor, connection)
+			raise error
+
+	inner.__name__ = function.__name__
+	return inner
+
+
+@connect
+def Persons_email_exists(cursor: psycopg2.extensions.cursor, email: str) -> bool:
+	"""
+	SUMMARY: Checks whether a Persons's email exists.
+	PARAMS:  The email to check.
+	DETAILS: Makes a query to the DB. Evaluates whether the email exists.
+	RETURNS: True is the email is found, false otherwise.
+	"""
+	cursor.execute("""SELECT * FROM "Persons";""")
+	[print(result) for result in cursor]
+
+	cursor.execute("""SELECT * FROM "Persons" WHERE "Email" = %s;""", (email,))
+	return cursor.rowcount > 0
+
+
+@connect
+def Persons_username_exists(cursor: psycopg2.extensions.cursor, username: str) -> bool:
+	"""
+	SUMMARY: Checks whether a Persons's email exists.
+	PARAMS:  The email to check.
+	DETAILS: Makes a query to the DB. Evaluates whether the email exists.
+	RETURNS: True is the email is found, false otherwise.
+	"""
+	cursor.execute("""SELECT * FROM "Persons" WHERE "Username" = %s;""", (username,))
+	return cursor.rowcount > 0
 
 
 # Adds a new user to the database, returns error message
-def add_new_user(request):
-	connection = new_database_connection()
-	connection.autocommit = True	# Set auto commit to false
-	cursor = connection.cursor()
+@connect
+def add_new_user(cursor: psycopg2.extensions.cursor, request: werkzeug.local.LocalProxy):
+	email: str = request.form["email"]
+	if(Persons_email_exists(email)):
+		raise Exception(f"There is already an account for email '{email}'.")
 
-	# Email and username already exist together in database
-	if user_exists(request):
-		return "There is already an account with this email and username. Please change one to create a new account."
+	username: str = request.form["uname"]
+	if(Persons_username_exists(username)):
+		raise Exception(f"There is already an account for username '{username}'.")
 
-	# Password and confirmed password do not match
 	if request.form["pass"] != request.form["confirmPass"]:
-		return "The password and confirmed password do not match. Please try again."
+		raise Exception("The password and confirmed password do not match. Please try again.")
 
-	cursor.execute("""INSERT INTO "Persons"("Email", "Username", "Password") VALUES (%s, %s, %s);""",
-		(request.form["email"], request.form["uname"], request.form["pass"]))
+	query = \
+	"""
+	INSERT INTO "Persons" ("Email", "Username", "Password")
+	  VALUES (%s, %s, %s)
+	  RETURNING "PersonID";
+	"""
+	cursor.execute(query, (email, username, request.form["pass"]))
+	print(cursor.statusmessage)
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-
-	user_id = cursor.fetchone()[0]
-	return None
+	user_id: int = cursor.fetchone()[0]
+	print(user_id)
+	#TODO: Get Person Object for user_id
+	return user_id
